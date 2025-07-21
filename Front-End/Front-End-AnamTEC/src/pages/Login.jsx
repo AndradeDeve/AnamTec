c6b078760c7e5388f4283eaf13deb41a7fec7621
WesSamy <weslley.samuel6@outlook.com>
2025-07-21 20:21:58 -0300

    Tela de Login estilizada e ajustada conforme layout

--- Front-End/Front-End-AnamTEC/src/pages/Login.jsx
+++ Front-End/Front-End-AnamTEC/src/pages/Login.jsx
@@ -2,26 +2,30 @@
 import React from 'react';
 import "./Login.css";
 import logoAnamTec from "../IMG/logoAnamTEC.png"
-function Login() {
-    
+
+export default function Login() {
   return (
-    <div className='top-layout' >
-        <header className="header">
-            <h1 className="TitleLogo">AnamTec</h1>
-            <img src={logoAnamTec} alt="Logo" className="logo" />
-      </header>
- <div className='bottom-layout'>
-        <h2 className='title'>Acesso ao Sistema</h2>
-      <form className='forms'>
-        <label>Digite seu Email</label>
-        <input className='input' type="text" placeholder="Email" />
-           <label>Digite sua Senha</label>
-        <input className='input' type="password" placeholder="Senha" />
-        <button className='btn-login' type="submit">Entrar</button>
-      </form>
+    <div className="login-container">
+      <div className="header">
+        <h1>AnamTec</h1>
+      <img src={logoAnamTec} alt="Logo" className="logo" />
+      </div>
+
+      <div className="login-card">
+        <div className="card-header">
+          <h2>Acesso ao Sistema</h2>
+        </div>
+        <div className="card-body">
+          <label htmlFor="email">Digite seu e-mail</label>
+          <input type="email" id="email" placeholder="E-mail" />
+
+          <label htmlFor="senha">Digite sua senha</label>
+          <input type="password" id="senha" placeholder="Senha" />
+
+          <button>ENTRAR</button>
+        </div>
+      </div>
     </div>
-   </div>
   );
 }
 
-export default Login;
